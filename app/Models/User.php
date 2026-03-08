59cf18d7222c71f721132d666856c8702c4cc1fc
Ricardohuiscaleo <ricardo.huiscaleo@gmail.com>
2026-03-08 08:24:44 -0300

    fix: add active column to users table to resolve 500 login error

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements FilamentUser, HasTenants
         'name',
         'email',
         'password',
+        'active',
     ];
 
     protected $hidden = [
@@ -36,6 +37,7 @@ class User extends Authenticatable implements FilamentUser, HasTenants
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'active' => 'boolean',
         ];
     }
 
