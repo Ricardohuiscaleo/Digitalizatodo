5b100be220c0e410495bd276b08717658fc6fae0
Ricardohuiscaleo <ricardo.huiscaleo@gmail.com>
2026-03-25 21:33:55 -0300

    Fix: next.config.ts options warnings for cleaner build

--- Applications/app-pwa/next.config.ts
+++ Applications/app-pwa/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'standalone',
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
