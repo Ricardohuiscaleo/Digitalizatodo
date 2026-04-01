a492c79522a8fa1bde67c2fc5cd391a90e498867
Ricardohuiscaleo <ricardo.huiscaleo@gmail.com>
2026-04-01 15:17:48 -0300

    fix(mp): fix mobile rendering container height and add visual versioning for cache debugging

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,19 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'info/*'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+
+    'allowed_origins' => ['http://localhost:3001', 'https://app.digitalizatodo.cl', 'https://admin.digitalizatodo.cl'],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => false,
+
+    'supports_credentials' => true,
 ];
